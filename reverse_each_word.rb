3cc2b5a21df6c00fcbe970442a516439c3f92549
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-10 13:44:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
+  new_array = []
   array.each do |e| 
-    sentence = e.reverse
-    puts sentence.to_s
+    new_array.push(e.reverse)
+    puts new_array.to_s
   end
   
 end
