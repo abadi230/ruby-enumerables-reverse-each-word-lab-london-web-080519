22c61d5e321788382523f74a00e7c6ac78523686
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-10 14:08:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,16 +7,5 @@ def reverse_each_word(sentence)
   end
   new_array.join(" ")
 end
- # expect_any_instance_of(Array).to receive(:collect).and_call_original
-    # reverse_each_word("Verifying that collect is being called.")
-# str = "hello, my name is Abadi"
 
-# def reverse_each_word(sentence)
-# to_array = str.split(" ")
-
-#   to_array.collect do |str|
-#     "#{str.reverse.join(" "} "
-#   end
-  
-# end
   
