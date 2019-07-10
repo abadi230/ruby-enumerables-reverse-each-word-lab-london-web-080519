5677cee996784e95f3d990c38f2d88acd74f903c
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-10 13:56:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,5 +10,8 @@ end
  # expect_any_instance_of(Array).to receive(:collect).and_call_original
     # reverse_each_word("Verifying that collect is being called.")
 str = w%["hello, my name is Abadi"]
+to_array = str.split(" ")
 
-str.collect do |str|
+to_array.collect do |str|
+  str.reverse
+  
